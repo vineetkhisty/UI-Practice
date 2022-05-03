ce58b496b48648bd7cb9de972a631c7fd471ea82
Vineet Khisty <vineetk2008@gmail.com>
2022-05-03 12:49:31 +0530

    Removed about app

--- Django/mysite/about/views.py
+++
@@ -1,7 +0,0 @@
-from django import http
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render ,HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse('Welcome to about page')
