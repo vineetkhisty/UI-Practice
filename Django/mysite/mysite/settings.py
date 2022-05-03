7b2bef6cf9a32069f4796735d995a99e4aaae0b1
Vineet Khisty <vineetk2008@gmail.com>
2022-05-03 13:14:59 +0530

    Reverted the changes made in setting file

--- Django/mysite/mysite/settings.py
+++ Django/mysite/mysite/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -125,5 +124,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Added Manually
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR , "static") 
+    BASE_DIR / "static"
 ]
