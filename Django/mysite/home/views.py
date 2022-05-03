d41fd742c0e49edc0998d454a08852875d594231
Vineet Khisty <vineetk2008@gmail.com>
2022-05-03 13:23:18 +0530

    Redirected blank page url to template index

--- Django/mysite/home/views.py
+++ Django/mysite/home/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse('This is home page')
+    return render(request,'index.html')
+    # return HttpResponse('This is home page')
 
 def about(request):
     return HttpResponse('This is about page')
