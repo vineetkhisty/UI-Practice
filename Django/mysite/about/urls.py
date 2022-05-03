0fd071234be394e500f5251d0cdcb3fd2737731e
Vineet Khisty <vineetk2008@gmail.com>
2022-05-03 12:43:13 +0530

    Added about page urls

---
+++ Django/mysite/about/urls.py
@@ -0,0 +1,8 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('about/',views.index, name='about')
+]
--- Django/mysite/about/views.py
+++ Django/mysite/about/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django import http
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render ,HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Welcome to about page')
