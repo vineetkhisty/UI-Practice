d988b99f8dd14db26ae6b3b07ea0c1def4837d4a
Vineet Khisty <vineetk2008@gmail.com>
2022-05-03 12:56:10 +0530

    added about and service endpoints

--- Django/mysite/home/urls.py
+++ Django/mysite/home/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("",views.index,name='home')
+    path("",views.index,name='home'),
+    path('about',views.about,name='about'),
+    path('service',views.service,name='service')
+
 ]
--- Django/mysite/home/views.py
+++ Django/mysite/home/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse('This is home page')
+
+def about(request):
+    return HttpResponse('This is about page')
+
+def service(request):
+    return HttpResponse('We have various services')
