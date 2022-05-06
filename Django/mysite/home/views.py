6991a2395d72bce0bf3accd309b7d317efa111ee
Vineet Khisty <vineetk2008@gmail.com>
2022-05-06 17:14:47 +0530

    Added alert functionality

--- Django/mysite/home/views.py
+++ Django/mysite/home/views.py
@@ -2,6 +2,7 @@ import email
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
@@ -25,5 +26,5 @@ def contact(request):
         message1 = request.POST.get('message')
         contact = Contact(name= name1,email=email1,subject=subject1,message=message1,date=datetime.today())
         contact.save()
-
+        messages.success(request, 'Your detail have been sent.')
     return render(request,'contact.html')
