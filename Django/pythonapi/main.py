22f1484821a73130ef99d95b78073a8d7492680c
Vineet Khisty <vineetk2008@gmail.com>
2022-05-26 12:51:05 +0530

    Added the route

--- Django/pythonapi/main.py
+++ Django/pythonapi/main.py
@@ -1,8 +1,15 @@
+# To start the app use the command:
+#   uvicorn <name of your main file>:<name of your FastAPI instance>
+# Imported the library
 from xmlrpc.client import FastMarshaller
 from fastapi import FastAPI
 
-app =FastAPI()
+# Created an instance of the FastAPI
+app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Created the route(Path operations)
+
+
+@app.get("/")  # Decorator(References the path when going through the url)
+def root():  # Root function
+    return {"message": "Hello World"}  # Returning the data
